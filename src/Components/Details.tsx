f8b6df950bc30a2ecd66897d20cd2d75d3278813
Olatunji praise <praiseolatunji22@gmail.com>
2025-04-04 18:59:55 -0700

    responsiveness

--- src/Components/Details.tsx
+++ src/Components/Details.tsx
@@ -14,24 +14,22 @@ function Details({ handleClick }: DetailsProps) {
 
   // Filter buttons handler
   const handleBtns = (word: string) => {
-    if (word === "All") {
-      setCategory(list);
-    } else {
-      setCategory(list.filter((item) => item.kind === word));
-    }
+    setCategory(
+      word === "All" ? list : list.filter((item) => item.kind === word)
+    );
     setActiveTab(word);
   };
 
   return (
     <section className="container mx-auto w-full bg-bgColor px-4 py-6">
       {/* Search and Filters Section */}
-      <section className="flex flex-col md:flex-row md:justify-between items-center mb-6">
+      <section className="flex flex-col md:flex-row md:justify-between items-center gap-4 mb-6">
         {/* Search Bar */}
-        <div className="relative w-full max-w-md h-11 mb-4 md:mb-0">
+        <div className="relative w-full max-w-md h-11">
           <input
             type="text"
             onChange={(event) => setQuery(event.target.value)}
-            className="w-full h-full py-4 pl-10 pr-4 text-base text-black rounded-lg border-2 border-black"
+            className="w-full h-full py-2 pl-10 pr-4 text-base text-black rounded-lg border-2 border-black focus:outline-none focus:ring-2 focus:ring-brandColor"
             placeholder="Search clothing..."
           />
           <FaSearch className="absolute left-4 top-1/2 transform -translate-y-1/2 text-lg text-black" />
@@ -43,7 +41,7 @@ function Details({ handleClick }: DetailsProps) {
             <button
               key={word}
               onClick={() => handleBtns(word)}
-              className={`px-4 py-2 md:w-24 rounded-lg text-lg border-2 transition ${
+              className={`px-4 py-2 md:w-24 rounded-lg text-lg border-2 transition-all ${
                 activeTab === word
                   ? "bg-brandColor text-white border-brandColor"
                   : "text-brandColor border-brandColor hover:bg-brandColor hover:text-white"
@@ -64,7 +62,9 @@ function Details({ handleClick }: DetailsProps) {
               : item.name.toLowerCase().includes(query.toLowerCase())
           )
           .map((item) => (
-            <Cards key={item.id} item={item} handleClick={handleClick} />
+            <div key={item.id} className="w-full">
+              <Cards item={item} handleClick={handleClick} />
+            </div>
           ))}
       </section>
     </section>
